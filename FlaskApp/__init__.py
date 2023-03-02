a5d220b59d6899d371a0c659e60db53e01bbf56a
Petra Carrion <petra@carrion.io>
2023-03-03 00:09:27 +0100

    Update

--- FlaskApp/__init__.py
+++ FlaskApp/__init__.py
@@ -2,7 +2,7 @@ from datetime import date, datetime
 import os
 from uuid import uuid4
 
-from azure.cosmos import ContainerProxy
+from azure.cosmos import ContainerProxy, DatabaseProxy
 from flask import Flask
 
 import azure.cosmos.cosmos_client as cosmos_client
@@ -16,27 +16,27 @@ COSMOS_KEY = os.environ.get('COSMOS_KEY', '${{ secrets.COSMOS_KEY }}')
 DATABASE_ID = 'consume'
 CONTAINER_ID = 'reading'
 
-client = cosmos_client.CosmosClient(COSMOS_HOST, {'masterKey': COSMOS_KEY} )
+client = cosmos_client.CosmosClient(COSMOS_HOST, {'masterKey': COSMOS_KEY})
 
-db = client.create_database_if_not_exists(id=DATABASE_ID)
-container: ContainerProxy = db.create_container_if_not_exists(id=CONTAINER_ID, partition_key=PartitionKey(path='/id', kind='Hash'))
+db: DatabaseProxy = client.create_database_if_not_exists(id=DATABASE_ID)
 
 
-def create_reading(reading_date, reading_value):
+def init_container(reset=False):
+    if reset:
+        db.delete_container(container)
+    return db.create_container_if_not_exists(id=CONTAINER_ID, partition_key=PartitionKey(path='/id', kind='Hash'))
+
+
+container: ContainerProxy = init_container()
+
+
+def create_reading(reading_timestamp, reading_value):
     reading = {
         'id': str(uuid4()),
-        'reading_date': reading_date,
+        'reading_timestamp': reading_timestamp,
         'reading_value': reading_value
     }
     container.create_item(body=reading)
-
-
-@app.route("/")
-def index():
-    return (
-        "Try /hello/Petra for parameterized Flask route.\n"
-        f"Try /module for module import guidance"
-    )
 
 
 @app.route("/create")
@@ -45,16 +45,53 @@ def create_item():
     return "Created"
 
 
+@app.route("/reset")
+def reset_items():
+    init_container(reset=True)
+    return "Reset"
+
+
 @app.route("/list")
 def list_items():
-    all_items = list(container.read_all_items(max_item_count=100))
+    all_items = list(container.read_all_items())
+    all_items = [{
+        'reading_timestamp': item['reading_timestamp'],
+        'reading_value': item['reading_value']
+    } for item in all_items]
     return all_items
 
 
-@app.route("/hello/<name>", methods=['GET'])
-def hello(name: str):
-    return f"hello {name}"
+@app.route("/")
+def index():
+    result = '''
+    <table>
+        <tr>
+            <th>Timestamp</th>
+            <th>Value</th>
+        </tr>
+    '''
+    all_items = list_items()
+    for item in all_items:
+        result += f'''
+        <tr>
+            <td>{item['reading_timestamp']}</td>
+            <td>{item['reading_value']}</td>
+        </tr>
+        '''
+    result += '</table>'
+    result += f'''
+    <form>
+        <input name="reading_value" type="number" min="45" value="45"/>
+        <input value="Add" type="submit" />
+    </form>
+    '''
+    result += f'''
+    <form>
+        <input value="Reset All" type="submit" />
+    </form>
+    '''
+    return result
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
