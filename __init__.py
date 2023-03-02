34e7489425a2c6413dd271b578d56b43b05a1cd1
Petra Carrion <petra@carrion.io>
2023-03-02 20:52:33 +0100

    Add azure-functions-app-python.yml

--- __init__.py
+++ __init__.py
@@ -1,6 +1,33 @@
+import logging
 import azure.functions as func
-from flaskapp import app
+from flask import Flask, request
+
+app = Flask(__name__)
 
 
-def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
-    return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+
+@app.route('/hi')
+def hi():
+    return 'Hi World!'
+
+
+@app.route('/hello')
+@app.route('/hello/<name>', methods=['POST', 'GET'])
+def hello(name=None):
+    return name != None and 'Hello, ' + name or 'Hello, ' + request.args.get('name')
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+    uri = req.params['uri']
+    with app.test_client() as c:
+        doAction = {
+            "GET": c.get(uri).data,
+            "POST": c.post(uri).data
+        }
+        resp = doAction.get(req.method).decode()
+        return func.HttpResponse(resp, mimetype='text/html')
