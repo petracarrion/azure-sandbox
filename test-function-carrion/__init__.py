c046360ac8152364ea6bcef606917b88ed8c30cd
Petra Carrion <petra@carrion.io>
2023-03-02 20:11:53 +0100

    Add azure-functions-app-python.yml

--- test-function-carrion/__init__.py
+++ test-function-carrion/__init__.py
@@ -1,25 +1,7 @@
-import logging
-
 import azure.functions as func
+from FlaskApp import app
 
-
-def main(req: func.HttpRequest) -> func.HttpResponse:
-    logging.info('Python HTTP trigger function processed a request.')
-
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
-
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
-        return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request "
-             "body for a personalized response.",
-             status_code=200
-        )
+def main(req: func.HttpRequest, context: func.Context) -> func.HttpResponse:
+    """Each request is redirected to the WSGI handler.
+    """
+    return func.WsgiMiddleware(app.wsgi_app).handle(req, context)
